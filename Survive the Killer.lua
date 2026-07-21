444b5e83f5f3d2507734dfa0656ee7e979cccef9
wayhead26-ops <wayhead26@gmail.com>
2026-07-22 02:19:15 +0800

    Update Survive the Killer.lua

--- Survive the Killer.lua
+++ Survive the Killer.lua
@@ -51,7 +51,30 @@ Window:CreateHomeTab({
 	DiscordInvite = "5hazzAq7B",
 	Icon = 1
 })
+----------------------------function
+local Noclip = nil
+local Clip = nil
 
+function noclip()
+	Clip = false
+	local function Nocl()
+		if Clip == false and game.Players.LocalPlayer.Character ~= nil then
+			for _,v in pairs(game.Players.LocalPlayer.Character:GetDescendants()) do
+				if v:IsA('BasePart') and v.CanCollide and v.Name ~= floatName then
+					v.CanCollide = false
+				end
+			end
+		end
+		wait(0.21) -- basic optimization
+	end
+	Noclip = game:GetService('RunService').Stepped:Connect(Nocl)
+end
+
+function clip()
+	if Noclip then Noclip:Disconnect() end
+	Clip = true
+end
+--------------------------
 local ESP = loadstring(game:HttpGet("https://kiriot22.com/releases/ESP.lua"))()
 ESP:Toggle(true)
 ESP.Tracers = false
@@ -60,6 +83,8 @@ ESP.Boxes = false
 ESP.Players = false
 ESP.TeamColor = false
 local setheal = false
+
+--------------------------------------
 
 local Tab = Window:CreateTab({
 	Name = "Main",
@@ -723,7 +748,6 @@ MiscTab:CreateButton({
         setfpscap(1000)
 	end
 })
-
 
 local ConfigTab = Window:CreateTab({
 	Name = "Config Tab",
